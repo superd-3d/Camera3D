b6d44f0682501be4ff94dad81a74875073477a87
fanghailong <HaiLong.Fang@Runmit.com>
2015-04-15 15:35:27 +0800

    New Project

---
+++ Camera3D/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera3D
+//
+//  Created by fanghailong on 15-4-15.
+//  Copyright (c) 2015年 superd3d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Camera3D/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Camera3D
+//
+//  Created by fanghailong on 15-4-15.
+//  Copyright (c) 2015年 superd3d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
